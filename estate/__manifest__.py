7367af06896a3613b24ab8caa34f989c3a30d474
ScytaleTleilax <andreivradulescu@gmail.comx>
2025-03-29 17:28:13 +0200

    [ADD] minimal scaffold required for custom module development

---
+++ estate/__manifest__.py
@@ -0,0 +1,38 @@
+{
+    'name': 'Estate Management',
+    'version': '1.0',
+    'category': 'Real Estate',
+    'sequence': 10,
+    'summary': 'Manage properties and real estate listings',
+    'website': 'https://github.com/ScytaleTleilax/odoo18_training',
+
+    'depends': [
+        'base',
+        'base_setup',
+    ],
+
+    'data': [
+        # 'security/crm_security.xml',
+        # 'security/ir.model.access.csv',
+
+        # 'data/crm_lead_merge_template.xml',
+
+        # 'views/calendar_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+
+    'assets': {
+        'web.assets_backend': [
+            # 'crm/static/src/**',
+            # ('remove', 'crm/static/src/views/forecast_graph/**'),
+            # ('remove', 'crm/static/src/views/forecast_pivot/**'),
+        ],
+        'web.assets_backend_lazy': [
+            # 'crm/static/src/views/forecast_graph/**',
+            # 'crm/static/src/views/forecast_pivot/**',
+        ],
+    },
+    'author': 'andreivradulescu@gmail.com',
+    'license': 'LGPL-3',
+}
