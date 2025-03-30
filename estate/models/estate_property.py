8f6340df00e60f3431e33ca119d974946d218677
ScytaleTleilax <andreivradulescu@gmail.comx>
2025-03-30 19:39:24 +0300

    [IMP]  EstateProperty model - added private method for seting default value of date_availability 3 months from creation date

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,16 +1,18 @@
+from datetime import datetime, timedelta
 from odoo import models, fields
 
 
 class EstateProperty(models.Model):
     _name = 'estate.estate_property'
 
+    active = fields.Boolean(string='Active', default=True),
     name = fields.Char(string='Property Name', required=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Date Availability')
+    date_availability = fields.Date(string='Date Availability', default=lamda self: self._get_default_availability_date())
     expected_price = fields.Float(string='Expected Price', required=True)
     selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Bedrooms')
+    bedrooms = fields.Integer(string='Bedrooms', default='2')
     living_area = fields.Integer(string='Living Area')
     facades = fields.Integer(string='Facades')
     garage = fields.Boolean(string='Garage')
@@ -38,3 +40,6 @@ class EstateProperty(models.Model):
         required=True,
         copy=False,
     )
+
+    def _get_default_availability_date(self):
+        return datetime.today() + timedelta(months=3)
